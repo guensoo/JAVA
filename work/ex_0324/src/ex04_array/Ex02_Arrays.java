e8443f8c800c734c0c78134b8a99d5e5e61addae
ChoiKeen <chbk9195@naver.com>
2025-03-25 09:46:17 +0900

    오타 수정

--- work/ex_0324/src/ex04_array/Ex02_Arrays.java
+++ work/ex_0324/src/ex04_array/Ex02_Arrays.java
@@ -119,7 +119,7 @@ public class Ex02_Arrays {
 		arr3[1] = new int[3];
 		arr3[2] = new int[1];
 		
-		int[][] iArr = new int[2][3];
+		int[][] iArr2 = new int[2][3];
 		
 		iArr2[0][0] = 1; 
 		iArr2[0][1] = 1;
