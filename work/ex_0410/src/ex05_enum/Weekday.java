5f85788842ba37bc54fd899bcdb77bf46aef2eb7
ghfkd12 <chbk9195@naver.com>
2025-04-10 18:21:20 +0900

    edit

--- work/ex_0410/src/ex05_enum/Weekday.java
+++ work/ex_0410/src/ex05_enum/Weekday.java
@@ -3,10 +3,4 @@ package ex05_enum;
 public enum Weekday {
 	MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,
 	SUNDAY;
-	
-	String day;
-	
-	private Weekday(String day) {
-		this.day = day;
-	}
 }
