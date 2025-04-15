d12704e9a9860da2cc5548edb8640765ca565520
ghfkd12 <chbk9195@naver.com>
2025-04-15 14:00:41 +0900

    4. 15.(Tue) Study

---
+++ work/ex_0414/src/ex01_reapeat/Programers.java
@@ -0,0 +1,19 @@
+package ex01_reapeat;
+
+import java.util.Scanner;
+
+public class Programers {
+
+}
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number % n == 0 && number % m == 0){
+         answer = 1;
+        }
+        else{
+            answer = 0;
+        }
+        return answer;
+    }
+}
